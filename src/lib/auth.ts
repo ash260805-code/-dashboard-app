8918103a7884cc0d5a13d7ec6218daebe3478dcc
Avinash P <ash9967668742@gmail.com>
2026-02-10 13:07:15 +0530

    Fix Prisma Client instantiation in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/db";
 import { authConfig } from "./auth.config";
-
-const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     ...authConfig,
