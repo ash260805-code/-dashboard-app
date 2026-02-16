ef776ef15b7e1c9923483a7c621dcd441739ec20
Avinash P <ash9967668742@gmail.com>
2026-02-16 22:05:56 +0530

    fix: bundle puppeteer-extra and deep deps to resolve serverless runtime errors

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: [
     "@sparticuz/chromium",
-    "puppeteer-core",
-    "puppeteer-extra",
-    "puppeteer-extra-plugin-stealth",
-    "merge-deep",
-    "clone-deep",
-    "is-plain-object",
-    "kind-of",
-    "shallow-clone",
-    "deep-extend"
+    "puppeteer-core"
   ],
 };
 
