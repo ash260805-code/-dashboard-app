fb2edc6956a6b4010e95effdb540d4d6e2a4ca68
Avinash P <ash9967668742@gmail.com>
2026-02-17 16:30:06 +0530

    fix(deps): downgrade chromium to v131 and cleanup unused deps

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: [
     "@sparticuz/chromium",
-    "puppeteer-core",
-    "puppeteer-extra-plugin-stealth",
-    "is-plain-object",
-    "merge-deep",
-    "clone-deep"
+    "puppeteer-core"
   ],
 };
 
